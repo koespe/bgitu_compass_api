6a0f013a851d89ac33d1d723b7b559ed70f5ab5a
koespe <kirillsportige@gmail.com>
2026-01-17 22:24:45 +0300

    Название группы теперь не uppercase

--- database/base.py
+++ database/base.py
@@ -18,12 +18,30 @@ async def db_init():
         await conn.run_sync(Base.metadata.create_all)
 
 
-async def manage_groups(group_name):
+async def manage_groups(group_name: str):
+    """
+    Изначально названия групп были uppercase, но для красивого интерфейса приложения теперь названия сохраняются
+     в оригинальном виде, внедряется это посреди года, так что приходится писать проверки лишние
+    """
     async with AsyncSession(bind=engine) as session:
-        group_id = (await session.execute(select(Groups.id).where(Groups.name == group_name))).scalar()
-        if group_id is not None:
+        # Ищем группу по верхнему регистру (для совместимости со старыми данными)
+        group_id = (await session.execute(select(Groups.id).where(Groups.name == group_name.upper()))).scalar()
+        
+        if group_id:
+            # Если нашли группу в верхнем регистре, обновляем имя на оригинальное (с сохранением регистра)
+            query = await session.execute(select(Groups).where(Groups.id == group_id))
+            group = query.scalar()
+            if group.name != group_name:
+                group.name = group_name
+                await session.commit()
             return group_id
         else:
+            # Ищем по оригинальному имени (на случай, если группа уже с правильным регистром)
+            group_id = (await session.execute(select(Groups.id).where(Groups.name == group_name))).scalar()
+            if group_id is not None:
+                return group_id
+            
+            # Создаем новую группу с оригинальным именем (с сохранением регистра)
             new_group = Groups(name=group_name)
             session.add(new_group)
             await session.commit()
